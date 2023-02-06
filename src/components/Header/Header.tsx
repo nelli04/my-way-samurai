5af481566986080922caa7da281bddd8e1474077
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-02-06 15:08:35 +0300

    01

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <header className='header'>
+            <img
+                src='https://fotorelax.ru/wp-content/uploads/2016/02/Beautiful-photos-and-pictures-on-different-topics-01.jpg'/>
+        </header>
+    );
+  }
