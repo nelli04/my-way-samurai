f1d9d2a75230e9a9e6882df42a25d223c6af7a77
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-02-06 15:09:54 +0300

    01

---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export function Profile () {
+    return (
+        <div className='content'>
+            <img
+                src='https://fotorelax.ru/wp-content/uploads/2016/02/Beautiful-photos-and-pictures-on-different-topics-05.jpg'/>
+            <div>ava</div>
+            <div>mypost</div>
+            <div>newpost</div>
+            <div>post1</div>
+        </div>
+    )
+}
