3fe0209d2241c57672c1f9309aa0108505248751
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-02-07 16:25:06 +0300

    02

---
+++ src/components/Dialogs/Dialogs.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import p from './dialogs.module.css'
+
+export function Dialogs () {
+    return (
+        <div className={p.content}>
+          Dialogs
+        </div>
+    )
+}
